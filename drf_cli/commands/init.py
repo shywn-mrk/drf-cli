c4ac70c30d93b39addb5c2a16f915d86e5c632df
Shayan Karimi <shayankarimi0078@gmail.com>
2021-02-23 13:32:40 +0330

    Removing the .git folder

--- drf_cli/commands/init.py
+++ drf_cli/commands/init.py
@@ -1,20 +1,56 @@
 import click
 import os
 import subprocess
-import shutil
+import platform
+
+OPERATING_SYSTEM = platform.system()
+
+def clone_repository(name):
+    """
+    Clones the reposity from git and removes the .git folder
+    """
+
+    repo_url = "https://github.com/shywn-mrk/drf-starter.git"
+
+    command = f"git clone {repo_url} {name}"
+    subprocess.run(command.split(), shell=True)
+
+    if OPERATING_SYSTEM == "Windows":
+        os.system(f"rmdir /s /q \"{name}/.git\"")
+    else:
+        os.system(f"rm -rf {name}/.git")
+
+# def install_requirements(name):
+#     """
+#     Creates a virtual environment and installs the requirements
+#     """
+
+#     parent = os.getcwd()
+#     full_path = os.path.join(parent, name)
+#     os.chdir(full_path)
+#     os.system("python -m venv env")
+
+#     if OPERATING_SYSTEM == "Windows":
+#         !!!!!!!!this does not work!!!!!!!!
+#         script_path = f"{full_path}/env/Scripts/activate"
+#         exec(open(script_path).read())
+#     else:
+#         print("Installing requirements is not implemented for Linux or MacOS. You have to install them manually")
+
+#     os.system("pip install -r requirements.txt")
+
 
 @click.command()
-@click.argument("project_name")
-def cli(project_name):
+@click.argument("name")
+def cli(name):
     """
     Initialize a new Django Rest Framework project
     """
 
     try:
-        repo_url = "https://github.com/shywn-mrk/drf-starter.git"
+        clone_repository(name)
 
-        command = f"git clone {repo_url} {project_name}"
-        subprocess.run(command.split(), shell=True)
+        # install_requirements(name)
 
         print("Project has been initialized successfuly!")
     except:
